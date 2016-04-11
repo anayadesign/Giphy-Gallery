fd3a966e4412d8afe136e888609eb50aa1c2b02a
AnayaDesign <davidanayadesign@gmail.com>
2016-04-10 17:54:33 -0700

    got 'em!
    • removed output var, using console.logs to verify functions and reqs
    • renamed urlBuild -> searchURL, searchKey
    • uncoupled search limit and searchKey
    • JSON parse SUCCESS!

--- js/main.js
+++ js/main.js
@@ -1,28 +1,31 @@
 //Global vars
 var textField = document.getElementById("search"), //search input
-    trigger = document.getElementById("launch"),  //launch button
-    output = document.getElementById("print");
+    trigger = document.getElementById("launch");  //launch button
 
 //Triggered by the button!
 function getGiphy() {
-  var searchLimit = "9",
-      urlBuild1 = "http://api.giphy.com/v1/gifs/search?q=",
-      urlBuild2 = "&api_key=dc6zaTOxFJmzC&limit=" + searchLimit,
+  var searchLimit = "&limit=9",
+      searchRating = "&rating=pg",
+      searchURL = "http://api.giphy.com/v1/gifs/search?q=",
+      searchKey = "&api_key=dc6zaTOxFJmzC",
       searchString = textField.value.toLowerCase(),
-      searchAPI = urlBuild1 + searchString.split(' ').join('+') + urlBuild2;
-  console.log(searchString);
-  console.log(searchAPI);
+      getGiphyAPI = searchURL + searchString.split(' ').join('+') + searchKey + searchLimit + searchRating;
+  console.log(getGiphyAPI);
 
 
   function reqListener() {
-    console.log("loading");
-    console.log(this.responseText);
+    console.log("LOADING");
+    var searchData = this.response,
+        searchObj = JSON.parse(searchData);
+
+    //console.log(searchData);
+    console.log(searchObj.data[0].bitly_url);//log a specific value, needs a loop from 0 through 8
     console.log("FINISHED");
   }
 
   //Get the data from Giphy API
   var oReq = new XMLHttpRequest();
   oReq.addEventListener("load", reqListener);
-  oReq.open("GET",searchAPI, true);
+  oReq.open("GET",getGiphyAPI, true);
   oReq.send();
 }
